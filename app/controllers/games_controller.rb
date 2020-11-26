8fc1705097b100873284aaef2de5561d4d04dc4e
Cristhiano Mello <cristhiano.f.mello@gmail.com>
2020-11-25 21:46:47 -0300

    include and english

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     letters = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
@@ -7,5 +10,19 @@ class GamesController < ApplicationController
     end
   end
 
-  def score; end
+  def score
+    @result = ''
+    @letters = params[:letters]
+    @word = params[:word].upcase
+    @include = @word.chars.all? { |letter| @word.count(letter) <= @letters.count(letter) }
+    @english = english?(params[:word])
+  end
+
+  private
+
+  def english?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
 end
